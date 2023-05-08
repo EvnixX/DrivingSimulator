04d740c9c25bb0ec7e711aecb695a8d38fd8a0b3
Julian2022DE <jdavid142@misena.edu.co>
2023-05-08 17:09:01 -0500

    carro movilidad basica lista

--- Racing prueba/Assets/Script/Controller.cs
+++ Racing prueba/Assets/Script/Controller.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public int Frenar = 500;
     public WheelCollider Rueda1;
     public WheelCollider Rueda2;
+    public WheelCollider Rueda3;
+    public WheelCollider Rueda4;
+    public Transform Llantas1;
+    public Transform Llantas2;
     public int Velocity = 150;
 
     private void Start()
@@ -15,8 +20,26 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
-        Rueda1.motorTorque = Velocity * Input.GetAxis("Vertical");
-        Rueda2.motorTorque = Velocity * Input.GetAxis("Vertical");
+        Llantas1.localEulerAngles = new Vector3(0, Rueda3.steerAngle, 0);
+        Llantas2.localEulerAngles = new Vector3(0, Rueda4.steerAngle, 0);
     }
 
+    private void FixedUpdate()
+    {
+        Rueda1.motorTorque = Velocity * Input.GetAxis("Vertical");
+        Rueda2.motorTorque = Velocity * Input.GetAxis("Vertical");
+        Rueda3.steerAngle = -40 * Input.GetAxis("Horizontal");
+        Rueda4.steerAngle = -40 * Input.GetAxis("Horizontal");
+
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            Rueda1.brakeTorque = Frenar;
+            Rueda2.brakeTorque = Frenar;
+        }
+        else
+        {
+            Rueda1.brakeTorque = 0;
+            Rueda2.brakeTorque = 0;
+        }
+    }
 }
