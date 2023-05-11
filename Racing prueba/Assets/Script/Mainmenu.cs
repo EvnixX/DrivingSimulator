9c70d13dcbeb52362194742a4bc8d21d17441eef
evnixx <evelynvmontano97@gmail.com>
2023-05-11 14:20:02 -0500

    Seguimos con interfaces

---
+++ Racing prueba/Assets/Script/Mainmenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+  public void Menu()
+  {
+        SceneManager.LoadScene("Menu");
+  }
+
+  public void Tutorial()
+  {
+       SceneManager.LoadScene("Controles");
+  }
+
+    public void Controles()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+}
